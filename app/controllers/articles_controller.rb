9b6e3d8687d79ddb10da05e9f200cee9c230effa
Arina <ul.arina09@gmail.com>
2017-10-11 09:48:52 +0300

    push to girhub

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+
+	def index
+		@articles = Article.all
+	end
+
+	def new
+	end
+
+	def create
+		#@article = Article.new(params.require(:article).permit(:title, :text))
+		@article = Article.new(article_params)
+		@article.user = User.first
+  		@article.save
+  		redirect_to @article
+  		#render plain: params[:article].inspect
+	end
+
+	def show
+    	@article = Article.find(params[:id])
+    	@user = @article.user
+	end
+
+	private
+
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
+
+end
