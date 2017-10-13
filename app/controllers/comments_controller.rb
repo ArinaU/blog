72654c0c42ed8d535c8c6b1372ef4c58858fed12
Yunna <j-june@yandex.ru>
2017-10-13 15:34:38 +0300

    fix comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,16 @@ class CommentsController < ApplicationController
 	load_and_authorize_resource
 
 	def create
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
-
-		redirect_to article_path(@article)
-	end
-
-	private
-
-	def comment_params
-    	params.require(:comment).permit(:body)
-  	end
-
+ 	  @article = Article.find(params[:article_id])
+ 	  @comment = @article.comments.build(comment_params)
+ 	  @comment.user = current_user
+ 	  @comment.save
+ 	  redirect_to article_path(@article)
+ 	end
+ 
+ 	private
+ 
+ 	def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
