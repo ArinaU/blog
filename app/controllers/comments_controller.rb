12447ddb90b1d6eb6fe0229cbf24f409223b9af5
Arina <ul.arina09@gmail.com>
2017-10-13 14:55:49 +0300

    Fixes bugs with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   
 	before_action :authenticate_user!
 	load_and_authorize_resource
-
+=begin
 	def create
 		@article = Article.find(params[:article_id])
 		@current_user = current_user
@@ -16,5 +16,19 @@ class CommentsController < ApplicationController
 	def comment_params
     	params.require(:comment).permit(:body)
   	end
+=end
+	def create
+	   @article = Article.find(params[:article_id])
+	   @comment = @article.comments.build(comment_params)
+	   @comment.user = current_user
+	   @comment.save
+	   redirect_to article_path(@article)
+	end
 
-end
+	private
+
+	def comment_params
+    	params.require(:comment).permit(:body)
+  	end
+
+ end
