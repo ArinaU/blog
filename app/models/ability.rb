867eba55072ea303d32b3d238e4988ac785384ed
Arina <ul.arina09@gmail.com>
2017-10-16 00:03:59 +0300

    eliminates the problem of deleting comments by editor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,9 @@ class Ability
         can :create, Article
         can :create, Comment
         can :destroy, Article, user_id: user.id
-        can :destroy, Comment, user_id: user.id
+        can :destroy, Comment do |comment|
+          comment.article_id == Article.find_by(user_id: user.id).id
+        end
         can :update, Article, user_id: user.id
         can :update, Comment, user_id: user.id
 
