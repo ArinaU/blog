63d6a90159b0532ce83776a258ba5426b81f5336
Yunna <j-june@yandex.ru>
2017-10-16 11:51:48 +0300

    shortens ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,36 +4,24 @@ class Ability
     def initialize(user)    
       user ||= User.new
       if user.role? :editor
-        #can :read, :all
-        can :read, Article
-        can :create, Article
-        # can :read, Comment do |comment|
-        #   user.id == comment.user_id or comment.is_deleted == false
-        # end
-
+        can [:read, :create], Article
+        cannot :read, @article => Comment
         can :create, Comment
-        can :destroy, Article, user_id: user.id
+        can [:update, :destroy], Article, user_id: user.id
         can :destroy, Comment do |comment|
           comment.article_id == Article.find_by(user_id: user.id).id
         end
-        can :update, Article, user_id: user.id
-        can :update, Comment, user_id: user.id
-        can :destroy, Comment, user_id: user.id
+        can [:update, :destroy], Comment, user_id: user.id
 
       elsif user.role? :reader
-        #can :read, :all
         can :read, Article
         can :create, Comment
-        can :update, Comment, user_id: user.id
-        can :destroy, Comment, user_id: user.id
-        # can :read, Comment do |comment|
-        #   user.id == comment.user_id or comment.is_deleted == false
-        # end
+        can [:update, :destroy], Comment, user_id: user.id
 
       else
         can :read, Article
 
-    end
+      end
   end
 end
 
