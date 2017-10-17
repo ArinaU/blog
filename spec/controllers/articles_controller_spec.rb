97438cb687e1e99c5723cd6a528ab33e8c00efd2
Arina <ul.arina09@gmail.com>
2017-10-17 15:08:32 +0300

    updates articles_controller_spec.rb

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -11,7 +11,23 @@ RSpec.describe ArticlesController do
                                article_id: @article.id)
     @comment2 = Comment.create(body: 'body2', user_id: @user.id,
                                article_id: @article.id)
+    sign_in @user
   end
+
+
+   describe 'POST#create' do
+
+    it 'creates a new article' do
+
+      expect {
+        post :create, params:{ article: { title: "title1", text: "text1" } }
+      }.to change(Article, :count).by(1)
+
+      expect(response).to redirect_to article_path(assigns[:article])
+    end
+  end
+
+
   describe 'GET index' do
     it 'renders index and comments count next to article' do
       get :index
@@ -19,4 +35,19 @@ RSpec.describe ArticlesController do
       expect(@article.comments.count).to eq(2)
     end
   end
+
+
+  describe 'GET#show' do
+    before { get :show, params: { id: @article.id } }
+
+    it 'assigns the requested article to the @article' do
+      expect(assigns(:article)).to eq(@article)
+    end
+
+    it 'assigns user of the current article to @user' do
+      expect(assigns(:user)).to eq(@article.user)
+    end
+  end
+
 end
+
